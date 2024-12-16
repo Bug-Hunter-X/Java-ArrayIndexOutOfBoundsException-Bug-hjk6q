033d7a058edc315eea3d23c39b7851e8b1f7f9fe
BugHunter-X <askzenai@gmail.com>
2024-12-16 09:04:05 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 2;
+        }
+        //To avoid the exception, either do not access arr[5] or make the array larger
+        //System.out.println(arr[5]);
+    }
+}
